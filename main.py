30ad78b9fd836e35d409cbfa7dfd980dab8197bf
Brage Lytskjold <bragelyt@gmail.com>
2021-10-22 12:16:10 +0200

    only run one search

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from sim.simpleBoatController import SimpleBoatController
 from ast.mcts.mctsController import MCTSController
 
 def main():
-    for i in range(50):  # Running 50 separate searches of size 50 000 each I got a crash 42/50 times. Might want to tune exploration biases? 
-        MCTS = MCTSController(2.0)
-        bestState = MCTS.loop()
+    MCTS = MCTSController(2.0)
+    bestState = MCTS.loop()
     SBC = SimpleBoatController(bestState)
     SBC.plot()
 
